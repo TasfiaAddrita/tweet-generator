dc2966f791573c40cf908ea3a18a35eccfcef50b
TasfiaAddrita <tasfia.addrita@gmail.com>
2019-11-03 22:06:51 -0800

    stretch: display n number of words

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from flask import Flask, render_template, request, redirect, url_for
 # from bson.objectid import ObjectId
 import os
 from datetime import datetime
-from src.histogram import *
 from src.sample import * 
 
 # local deployment
@@ -13,11 +12,22 @@ from src.sample import *
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    words_list = get_words('text/test.txt')
-    random_word = sample(words_list)
-    return render_template('index.html', word=random_word)
+@app.route('/', methods=['GET', 'POST'])
+def random_words(words=[]):
+    if request.method == 'GET':
+        return render_template('index.html')
+    else:
+        num_words = request.form.get('num')
+        if num_words == '':
+            num_words = 1
+        else:
+            num_words = int(num_words)
+        word_distribution = get_word_distribution(get_words('text/test.txt'))
+        random_words_list = []
+        for _ in range(num_words):
+            random_word = sample(word_distribution)
+            random_words_list.append(random_word)
+        return render_template('index.html', words_list=random_words_list)
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.environ.get('PORT', 5000))
--- src/sample.py
+++ src/sample.py
@@ -1,78 +1,41 @@
 import random
-# import histogram
-from src.histogram import histogram_dict
-import sys
+import re
+import string
 
-def random_word(source_text):
-    words_list = histogram.get_words(source_text)
-    return random.choice(words_list)
+def get_words(source_text):
+    text = open(source_text)
+    story = text.read()
+    text.close()
 
-def get_freq_percent(word_occurance):
-    return word_occurance / len(words_list)
+    # regex help https://stackoverflow.com/a/13184791/12049271
+    delimiters = ' ', '\n'
+    regexPattern = '|'.join(map(re.escape, delimiters))
+    words = re.split(regexPattern, story)
+    words = list(filter(None, [word.strip(string.punctuation).lower() for word in words]))
+    return words
+
+def histogram_dict(words):
+    histogram = {}
+    for word in words:
+        if word not in histogram:
+            histogram[word] = 1
+        else:
+            histogram[word] += 1
+    return histogram
 
 def get_word_distribution(words_list):
     histo = histogram_dict(words_list)
     word_distribution = {}
     range_start = 0
     for word in histo:
-        # freq_percent = get_freq_percent(histo[word])
         freq_percent = histo[word] / len(words_list)
         word_distribution[word] = [range_start, range_start + freq_percent]
         range_start += freq_percent
     
     return word_distribution
 
-def sample(words_list):
-    word_distribution = get_word_distribution(words_list)
+def sample(word_distribution):
     ran_num = random.random()
     for word in word_distribution:
         if word_distribution[word][0] <= ran_num <= word_distribution[word][1]:
             return word
-
-def test(words_list):
-    test = []
-    word_distribution = get_word_distribution(words_list)
-    print(word_distribution, '\n')
-    for _ in range(1000):
-        test.append(sample(words_list))
-    hist_ran_words = histogram.histogram_dict(test)
-    print(hist_ran_words, '\n')
-    for word in hist_ran_words:
-        print(word, 'test', hist_ran_words[word]/1000, 'dist', round(get_freq_percent(histogram.histogram_dict(words_list)[word]), 3))    
-    return
-
-if __name__ == '__main__':
-
-    # words_list = histogram.get_words('test.txt')
-    # test(words_list)
-
-    params = sys.argv[1:]
-    words_list = histogram.get_words(params[0])
-    # print(sample(words_list))
-    print(test(words_list))
-
-# ------ bad code ------
-# def get_freq_percent(word_occurance):
-#     return round(word_occurance / len(words_list) * 100)
-
-# def get_word_distribution(words_list):
-#     histo = histogram.histogram_dict(words_list)
-#     word_distribution = {}
-#     range_start = 0
-#     for word in histo:
-#         freq_percent = get_freq_percent(histo[word])
-#         word_distribution[word] = (freq_percent, list(range(range_start, range_start + freq_percent + 1)))
-#         range_start += freq_percent
-    
-#     # print(word_distribution)
-#     return word_distribution
-
-# def sample(words_list):
-#     word_distribution = get_word_distribution(words_list)
-
-#     ran_num = random.randint(0, 100)
-#     for word in word_distribution:
-#         if ran_num in word_distribution[word][1]:
-#             return word
-#         # else:
-#         #     return (None, ran_num) # ask Alan why expression doesn't think ran_num is in word_distribution
